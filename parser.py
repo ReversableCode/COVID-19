54eaa3d902f33c9a111abf3bc386bf1585bf4d31
n4bz <tantanobel@gmail.com>
2020-03-23 04:22:10 +0100

    Added parser + parsed files

---
+++ parser.py
@@ -0,0 +1,17 @@
+import csv, json
+import time
+import re 
+import os
+
+dir = "/Users/Nab/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/"
+
+for file in os.listdir(dir):
+	filename = "%s%s" % (dir, file)
+	csv_path = open(filename, 'r')
+	json_path = open(filename + '.json', 'w')
+
+	fields = ("Country/Region", "Confirmed", "Deaths", "Recovered")
+
+	reader = csv.DictReader(csv_path, fields)
+	out = json.dumps( [ row for row in reader ] )
+	json_path.write(out)
